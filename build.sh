3147d35697e0d02bf2a3bfc66b7338ef72138c74
howinator <howiebenefiel@gmail.com>
2019-01-21 22:06:44 -0600

    Initial working deployment

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker build -t howinator/personal-blog .
+docker push howinator/personal-blog
---
+++ content/posts/my-first-post.md
@@ -0,0 +1,6 @@
+---
+title: "My First Post"
+date: 2019-01-21T21:00:29-06:00
+draft: true
+---
+
