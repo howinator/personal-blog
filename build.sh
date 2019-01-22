adf17bc61893330ce419270acea7a9ede1140aae
howinator <howiebenefiel@gmail.com>
2019-01-21 22:29:52 -0600

    Version to v2

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ git fetch --tags
 new_version=$((1 + $(git tags | ggrep -P "v\d+" | sed 's/v//g' | sort -n | tail -n 1)))
 git add -A
 git commit -S -m "Version to v$new_version"
-git tag "$new_version"
+git tag "v$new_version"
 docker push origin
 docker push --tags
 
