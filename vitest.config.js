aeaba55fcc4db21090b927dcf44c6bbe43d5adab
Howie Benefiel <5847863+howinator@users.noreply.github.com>
2026-02-15 16:56:07 -0800

    Move everything to top level dir (#8)

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['assets/js/**/*.test.js'],
+    include: ['site/assets/js/**/*.test.js'],
     environment: 'jsdom',
   },
 });
